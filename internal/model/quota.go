fe0535fb2fac9c4f36c85e97980061f6127b2e22
Sarah Roberts <sarahr@cyverse.org>
2022-10-18 14:24:15 -0700

    CORE-1800: add the last modified time to usages and quotas

--- internal/model/quota.go
+++ internal/model/quota.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 // Quota define the structure for an Api Plan and Quota.
 type Quota struct {
 	ID             *string      `gorm:"type:uuid;default:uuid_generate_v1()" json:"id"`
@@ -7,6 +9,7 @@ type Quota struct {
 	UserPlanID     *string      `gorm:"type:uuid;not null" json:"-"`
 	ResourceTypeID *string      `gorm:"type:uuid;not null" json:"-"`
 	ResourceType   ResourceType `json:"resource_type"`
+	LastModifiedAt *time.Time   `json:"last_modified_at"`
 }
 
 // TableName specifies the table name to use the database.
