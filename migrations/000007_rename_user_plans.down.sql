8298cb39c8c860f36304f930c7a4c1ef6641ad30
John Wregglesworth <wregglej@cyverse.org>
2023-01-12 15:07:05 -0700

    Rename triggers on the subscriptions table

--- migrations/000007_rename_user_plans.down.sql
+++ migrations/000007_rename_user_plans.down.sql
@@ -3,5 +3,9 @@ BEGIN;
 SET search_path = public, pg_catalog;
 
 ALTER TABLE IF EXISTS subscriptions RENAME TO user_plans;
+ALTER TRIGGER IF EXISTS subscriptions_last_modified_at_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_at_trigger;
+ALTER TRIGGER IF EXISTS subscriptions_last_modified_by_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_by_trigger;
+ALTER TRIGGER IF EXISTS subscriptions_last_modified_by_insert_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_by_insert_trigger;
+ALTER TRIGGER IF EXISTS subscriptions_created_by_trigger ON TABLE user_plans RENAME TO user_plans_created_by_trigger;
 
 COMMIT;
--- migrations/000007_rename_user_plans.up.sql
+++ migrations/000007_rename_user_plans.up.sql
@@ -3,5 +3,9 @@ BEGIN;
 SET search_path = public, pg_catalog;
 
 ALTER TABLE IF EXISTS user_plans RENAME TO subscriptions;
+ALTER TRIGGER IF EXISTS user_plans_last_modified_at_trigger ON TABLE subscriptions RENAME TO subscriptions_last_modified_at_trigger;
+ALTER TRIGGER IF EXISTS user_plans_last_modified_by_trigger ON TABLE subscriptions RENAME TO subscriptions_last_modified_by_trigger;
+ALTER TRIGGER IF EXISTS user_plans_last_modified_by_insert_trigger ON TABLE subscriptions RENAME TO subscriptions_last_modified_by_insert_trigger;
+ALTER TRIGGER IF EXISTS user_plans_created_by_trigger ON TABLE subscriptions RENAME TO subscriptions_created_by_trigger;
 
 COMMIT;
