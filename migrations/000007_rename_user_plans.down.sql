51891d69168b82815ef0ee8cd2f1d2c785fe2150
John Wregglesworth <wregglej@cyverse.org>
2023-01-12 15:27:39 -0700

    Rename foreign keys into subscriptions to subscription_id from user_plan_id.

--- migrations/000007_rename_user_plans.down.sql
+++ migrations/000007_rename_user_plans.down.sql
@@ -3,9 +3,15 @@ BEGIN;
 SET search_path = public, pg_catalog;
 
 ALTER TABLE IF EXISTS subscriptions RENAME TO user_plans;
-ALTER TRIGGER IF EXISTS subscriptions_last_modified_at_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_at_trigger;
-ALTER TRIGGER IF EXISTS subscriptions_last_modified_by_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_by_trigger;
-ALTER TRIGGER IF EXISTS subscriptions_last_modified_by_insert_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_by_insert_trigger;
-ALTER TRIGGER IF EXISTS subscriptions_created_by_trigger ON TABLE user_plans RENAME TO user_plans_created_by_trigger;
+ALTER TRIGGER subscriptions_last_modified_at_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_at_trigger;
+ALTER TRIGGER subscriptions_last_modified_by_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_by_trigger;
+ALTER TRIGGER subscriptions_last_modified_by_insert_trigger ON TABLE user_plans RENAME TO user_plans_last_modified_by_insert_trigger;
+ALTER TRIGGER subscriptions_created_by_trigger ON TABLE user_plans RENAME TO user_plans_created_by_trigger;
+
+ALTER TABLE IF EXISTS quotas RENAME COLUMN subscription_id TO user_plan_id;
+ALTER INDEX IF EXISTS quotas_resource_type_subscription_index RENAME TO quotas_resource_type_user_plan_index;
+
+ALTER TABLE IF EXISTS usages RENAME COLUMN subscription_id TO user_plan_id;
+ALTER INDEX IF EXISTS usages_resource_type_subscription_index RENAME TO usages_resource_type_user_plan_index;
 
 COMMIT;
--- migrations/000007_rename_user_plans.up.sql
+++ migrations/000007_rename_user_plans.up.sql
@@ -8,4 +8,10 @@ ALTER TRIGGER IF EXISTS user_plans_last_modified_by_trigger ON TABLE subscriptio
 ALTER TRIGGER IF EXISTS user_plans_last_modified_by_insert_trigger ON TABLE subscriptions RENAME TO subscriptions_last_modified_by_insert_trigger;
 ALTER TRIGGER IF EXISTS user_plans_created_by_trigger ON TABLE subscriptions RENAME TO subscriptions_created_by_trigger;
 
+ALTER TABLE IF EXISTS quotas RENAME COLUMN user_plan_id to subscription_id;
+ALTER INDEX IF EXISTS quotas_resource_type_user_plan_index RENAME TO quotas_resource_type_subscription_index;
+
+ALTER TABLE IF EXISTS usages RENAME COLUMN user_plan_id to subscription_id;
+ALTER INDEX IF EXISTS usages_resource_type_user_plan_index RENAME TO usages_resource_type_subscription_index;
+
 COMMIT;
